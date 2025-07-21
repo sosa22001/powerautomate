65a80edc555e2f6cca86d275f3a1c146c7a2e4ea
Elmer Sosa <esosa@goconsultores.com>
2025-07-21 15:31:18 -0600

    index

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import webhookRouter from './routes/webhook.router.js'
+import webhookRouter from './src/routes/webhook.router.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.listen(4000, () =>{
-    console.log('Server Listen on port 4000')
-})
 
 app.use('/webhook', webhookRouter)
+
+
+export default app
